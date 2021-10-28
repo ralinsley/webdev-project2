894963139dd1ef3c6d465c72aeb1ab3713f4a60d
linslera <linslera@dukes.jmu.edu>
2021-10-28 11:10:47 -0400

    fix to schema

--- schema.sql
+++ schema.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS secrets;
 
 CREATE TABLE secrets (
     id SERIAL PRIMARY KEY,
-    secret_type VARCHAR,
-    'secret' VARCHAR(20)
+    secret_type VARCHAR(20),
+    'secret' TEXT
 );
