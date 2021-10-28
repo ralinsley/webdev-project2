1d2c7d95f6477a72e0a9fb11dbf2e6dcddf3886b
linslera <linslera@dukes.jmu.edu>
2021-10-28 10:38:38 -0400

    fix collate command

--- db-init.sql
+++ db-init.sql
@@ -1,4 +1,4 @@
 -- Create Unforget database and user. Ensure Unicode is fully supported.
-CREATE DATABASE IF NOT EXISTS project2 CHARACTER SET utf8mb4 COLLATE utf8mb4_09>
+CREATE DATABASE IF NOT EXISTS project2 CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 GRANT ALL PRIVILEGES ON project2.* TO p2_user@localhost;
 
